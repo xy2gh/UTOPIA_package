d11a6412cff0765416ce08863812db38fd74f4da
Prado Domercq <prado.domercq@gmail.com>
2025-10-31 10:46:15 +0100

    added new pakage name to innit even if I keep utopia as folder name I need to modify to match the pakg name in to´ml file

--- src/utopia/__init__.py
+++ src/utopia/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
-from importlib.metadata import version
-__version__ = version("utopia")
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("utopia-pkg")  # match your project name in pyproject.toml
+except PackageNotFoundError:
+    __version__ = "unknown"
